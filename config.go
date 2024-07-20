6df56ea4e6ab3cc7c2c900cf28e94e0c654516b1
Matt <1428380+grokbeer@users.noreply.github.com>
2024-07-20 04:12:46 +0000

    Added command-line flags for A/AAAA record filtering

--- config.go
+++ config.go
@@ -20,6 +20,8 @@ type config struct {
 	BindAddr string   `arg:"-a,--addr" default:"localhost" help:"address to bind on" env:"BIND"`
 	Port     uint16   `arg:"-p" default:"53" help:"port to bind on" env:"PORT"`
 	Debug    bool     `arg:"-v" default:"false" help:"also include debug information"`
+    v4Only   bool     `arg:"-4" help:"only support IPv4 (A) queries"`
+    v6Only   bool     `arg:"-6" help:"only support IPv6 (AAAA) queries"`
 }
 
 func parseArgs(logger *logrus.Logger) (*config, error) {
@@ -27,6 +29,12 @@ func parseArgs(logger *logrus.Logger) (*config, error) {
 	arg.MustParse(cfg)
 	if len(cfg.Domains) == 0 {
 		cfg.Domains = defaultDomains
+	}
+	// Logic to handle conflicting arguments, if necessary
+	if cfg.v4Only && cfg.v6Only {
+		logger.Warn("Both IPv4 and IPv6 support enabled. This is the default behavior.")
+		cfg.v4Only = false
+		cfg.v6Only = false
 	}
 	configureLogger(logger, cfg)
 	logger.WithField("config", cfg).Debug("config parsed")
