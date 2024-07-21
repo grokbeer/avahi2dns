d353863fb6300a1eeac12bad5cc92e13a28b5a34
Matt <1428380+grokbeer@users.noreply.github.com>
2024-07-21 12:25:38 +0000

    feat: Update config struct to include environment variable support
    The `config` struct has been updated to include environment variable support for the `Domains` and `Debug` fields. This allows the values of these fields to be set via environment variables in addition to command-line arguments.

--- config.go
+++ config.go
@@ -16,13 +16,13 @@ var defaultDomains = []string{
 }
 
 type config struct {
-	Domains     []string `arg:"-d" help:"comma-separated list of domains to resolve"`
+	Domains     []string `arg:"-d,env" help:"comma-separated list of domains to resolve"`
 	BindAddr    string   `arg:"-a,--addr,env:BIND" default:"localhost" help:"address to bind on"`
 	Port        uint16   `arg:"-p,env" default:"53" help:"port to bind on"`
 	IPv4Only    bool     `arg:"-4,env:IPV4_ONLY" default:"false" help:"only support IPv4 (A) queries"`
 	IPv6Only    bool     `arg:"-6,env:IPV6_ONLY" default:"false" help:"only support IPv6 (AAAA) queries"`
 	TimeoutSecs uint8    `arg:"-t,--timeout,env:TIMEOUT" default:"0" help:"timeout for avahi requests (in seconds, 0 for default avahi timeout)"`
-	Debug       bool     `arg:"-v" default:"false" help:"also include debug information"`
+	Debug       bool     `arg:"-v,env" default:"false" help:"also include debug information"`
 }
 
 func parseArgs(logger *logrus.Logger) (*config, error) {
